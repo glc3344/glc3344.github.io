5a9ea1a785048f2f3039a7e3101adffdb9c4735c
glc3344 <glc3344@gmail.com>
2017-02-16 09:37:16 -0500

    KnockoutJS basic setup
    Feat: Implemented knockoutJS view model.  Added a sidebar and made
    minor css changes

--- js/app.js
+++ js/app.js
@@ -90,3 +90,12 @@ function initMap() {
   //   map: map
   // });
 }
+
+// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
+function AppViewModel() {
+    this.firstName = "Bert";
+    this.lastName = "Bertington";
+}
+
+// Activates knockout.js
+ko.applyBindings(new AppViewModel());
